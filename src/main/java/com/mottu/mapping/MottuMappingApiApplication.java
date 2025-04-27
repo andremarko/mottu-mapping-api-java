56fa4613b0f801258206c9870384760f77e2b085
andremarko <151890840+andremarko@users.noreply.github.com>
2025-04-27 19:05:37 -0300

    first commit

---
+++ src/main/java/com/mottu/mapping/MottuMappingApiApplication.java
@@ -0,0 +1,13 @@
+package com.mottu.mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MottuMappingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MottuMappingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mottu/mapping/MottuMappingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mottu.mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MottuMappingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
