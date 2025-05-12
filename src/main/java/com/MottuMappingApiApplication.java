b307779d671071c3a260c6ce8eab62c7ea9dd42b
andremarko <151890840+andremarko@users.noreply.github.com>
2025-05-12 16:52:52 -0300

    Remoção da classe 'Country', movendo classes para pasta api (boa prática)

--- src/main/java/com/MottuMappingApiApplication.java
+++ src/main/java/com/MottuMappingApiApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MottuMappingApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MottuMappingApiApplication.class, args);
-	}
-
-}
+	}}
