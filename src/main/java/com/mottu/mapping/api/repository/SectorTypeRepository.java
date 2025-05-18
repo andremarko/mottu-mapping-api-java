7e5eb7e56b7dffcd8b6aebf2e27ae148167e6fc1
andremarko <andre.marcolongo@gmail.com>
2025-05-18 19:23:43 -0300

    chore: remove classes e DTOs obsoletos (Unit e SectorType)

--- src/main/java/com/mottu/mapping/api/repository/SectorTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mottu.mapping.api.repository;
-
-import com.mottu.mapping.api.model.SectorType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SectorTypeRepository extends JpaRepository<SectorType, Long> {
-}
--- src/main/java/com/mottu/mapping/api/repository/UnitRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mottu.mapping.api.repository;
-
-import com.mottu.mapping.api.model.Unit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UnitRepository extends JpaRepository<Unit, Long> {
-}
