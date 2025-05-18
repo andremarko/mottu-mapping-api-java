c1e8a3d894f6d9e33ff01983cd7d59a429c954b2
andremarko <andre.marcolongo@gmail.com>
2025-05-18 19:24:42 -0300

    refactor(moto): atualiza controller e service para usar DTOs aninhados

--- src/main/java/com/mottu/mapping/api/controller/MotoController.java
+++ src/main/java/com/mottu/mapping/api/controller/MotoController.java
@@ -1,4 +1,65 @@
 package com.mottu.mapping.api.controller;
 
+import com.mottu.mapping.api.dto.request.MotoRequestDTO;
+import com.mottu.mapping.api.dto.response.ModelResponseDTO;
+import com.mottu.mapping.api.dto.response.MotoResponseDTO;
+import com.mottu.mapping.api.dto.response.MotoYardResponseDTO;
+import com.mottu.mapping.api.dto.response.SectorResponseDTO;
+import com.mottu.mapping.api.exception.ModelNotFoundException;
+import com.mottu.mapping.api.exception.SectorNotFoundException;
+import com.mottu.mapping.api.model.Moto;
+import com.mottu.mapping.api.repository.ModelRepository;
+import com.mottu.mapping.api.repository.MotoRepository;
+import com.mottu.mapping.api.repository.SectorRepository;
+import com.mottu.mapping.api.service.MotoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/motos")
 public class MotoController {
+    @Autowired
+    private MotoService motoService;
+
+    @PostMapping
+    public ResponseEntity<MotoResponseDTO> create(@Valid @RequestBody MotoRequestDTO dto) {
+        Moto moto = motoService.save(dto);
+        return ResponseEntity.ok(toMotoResponseDTO(moto));
+    }
+
+
+
+
+    private MotoResponseDTO toMotoResponseDTO(Moto moto) {
+        ModelResponseDTO modelDTO = new ModelResponseDTO(
+                moto.getModel().getModelId(),
+                moto.getModel().getModelName()
+        );
+
+        MotoYardResponseDTO yardDTO = new MotoYardResponseDTO(
+                moto.getSector().getYard().getYardId(),
+                moto.getSector().getYard().getDescription(),
+                moto.getSector().get
+        );
+
+        SectorResponseDTO sectorDTO = new SectorResponseDTO(
+                moto.getSector().getId(),
+                yardDTO,
+                moto.getSector().getName(),
+                moto.getSector().getDescription(),
+                moto.getSector().getColorRgb(),
+                moto.getSector().getColorName()
+        );
+
+        return new MotoResponseDTO(
+                moto.getId(),
+                moto.getPlate(),
+                moto.getCoordinates(),
+                modelDTO,
+                sectorDTO
+        );
+    }
 }
--- src/main/java/com/mottu/mapping/api/service/MotoService.java
+++ src/main/java/com/mottu/mapping/api/service/MotoService.java
@@ -1,21 +1,84 @@
 package com.mottu.mapping.api.service;
 
+import com.mottu.mapping.api.dto.request.MotoRequestDTO;
+import com.mottu.mapping.api.dto.response.ModelResponseDTO;
+import com.mottu.mapping.api.dto.response.MotoResponseDTO;
+import com.mottu.mapping.api.exception.ModelNotFoundException;
+import com.mottu.mapping.api.exception.SectorNotFoundException;
+import com.mottu.mapping.api.model.Model;
 import com.mottu.mapping.api.model.Moto;
+import com.mottu.mapping.api.model.Sector;
+import com.mottu.mapping.api.repository.ModelRepository;
 import com.mottu.mapping.api.repository.MotoRepository;
+import com.mottu.mapping.api.repository.SectorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MotoService {
     @Autowired
     private MotoRepository motoRepository;
 
-    public Moto save(Moto moto) {
-        return motoRepository.save(moto);
+    @Autowired
+    private ModelRepository modelRepository;
+
+    @Autowired
+    private SectorRepository sectorRepository;
+
+
+    public MotoService(MotoRepository motoRepository, ModelRepository modelRepository, SectorRepository sectorRepository) {
+        this.motoRepository = motoRepository;
+        this.modelRepository = modelRepository;
+        this.sectorRepository = sectorRepository;
     }
 
 
+    public Moto save(MotoRequestDTO dto) {
+        Model model = modelRepository.findById(dto.getModelId())
+                .orElseThrow(() -> new ModelNotFoundException(dto.getModelId()));
 
+        Sector sector = sectorRepository.findById(dto.getSectorId())
+                .orElseThrow(() -> new SectorNotFoundException(dto.getSectorId()));
+
+        Moto moto = new Moto(dto.getPlate(), dto.getCoordinates(), model, sector);
+        return motoRepository.save(moto);
+    }
+
+    public List<Moto> readAll() {
+        List<Moto> motos = motoRepository.findAll();
+        if (motos.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No motos found");
+        }
+        return motos;
+    }
+
+    public Moto read(Long motoId) {
+        return motoRepository.findById(motoId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Moto not found with id: " + motoId));
+    }
+
+    public Moto update(Moto moto) {
+        Moto existingMoto = motoRepository.findById(moto.getMotorcycleId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Moto not found with id: " + moto.getMotorcycleId()));
+
+        existingMoto.setPlate(moto.getPlate());
+        existingMoto.setCoordinates(moto.getCoordinates());
+        existingMoto.setModel(moto.getModel());
+        existingMoto.setSector(moto.getSector());
+
+        return motoRepository.save(existingMoto);
+    }
+
+    public void delete(Long motoId) {
+        if (!motoRepository.existsById(motoId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Moto not found with id: " + motoId);
+        }
+        motoRepository.deleteById(motoId);
+    }
 }
