c589a6f2ac6ad140adf7ab846080283fa0987579
andremarko <151890840+andremarko@users.noreply.github.com>
2025-05-18 23:46:38 -0300

    chore(model): ajusta imports no modelo 'Model'

--- src/main/java/com/mottu/mapping/api/model/Model.java
+++ src/main/java/com/mottu/mapping/api/model/Model.java
@@ -1,6 +1,11 @@
 package com.mottu.mapping.api.model;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Data;
 
 @Entity
 @Data
@@ -8,8 +13,10 @@ import lombok.*;
 public class Model {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     @Column(name="model_id")
     private Long modelId;
+
     @Column(nullable = false)
     private String modelName;
 
