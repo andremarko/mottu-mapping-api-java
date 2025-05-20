6fcd5ed1dbc43a024ac52c9763069f07b12efb94
andremarko <151890840+andremarko@users.noreply.github.com>
2025-05-20 17:25:25 -0300

    feat(repository): adiciona métodos de busca por placa, setor e ambos combinados no MotoRepository

--- src/main/java/com/mottu/mapping/api/repository/MotoRepository.java
+++ src/main/java/com/mottu/mapping/api/repository/MotoRepository.java
@@ -1,6 +1,13 @@
 package com.mottu.mapping.api.repository;
 
 import com.mottu.mapping.api.model.Moto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MotoRepository extends JpaRepository<Moto, Long> {}
+public interface MotoRepository extends JpaRepository<Moto, Long> {
+    Page<Moto> findByPlate(String plate, Pageable pageable);
+    Page<Moto> findBySector_SectorId(Long sectorId, Pageable pageable);
+    Page<Moto> findByPlateAndSector_SectorId(String plate, Long sectorId, Pageable pageable);
+
+}
