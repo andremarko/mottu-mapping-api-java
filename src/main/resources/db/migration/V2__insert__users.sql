6dc325c00488906fa458d5897eaadb5246e883b1
andremarko <151890840+andremarko@users.noreply.github.com>
2025-09-28 19:31:35 -0300

    chore(db): adiciona scripts de migração e inserts iniciais para usuários, roles e permissões

---
+++ src/main/resources/db/migration/V2__insert__users.sql
@@ -0,0 +1,5 @@
+-- senha: admin123
+INSERT INTO TB_USER (username, password, role) VALUES ('admin', '$2b$12$D2Y5FFudE/vyv.3sti56puJH/5sOgApmmuUnxTki2A8Si.o7jsohu', 'ROLE_ADMIN');
+
+-- senha: oper123
+INSERT INTO TB_USER (username, password, role) VALUES ('operator', '$2b$12$uNSi53Foe.BXvb2r3mR1..Mc.cZ8ip5SIAuyZin028qAFR5FGQrQ6', 'ROLE_OPERATOR');
