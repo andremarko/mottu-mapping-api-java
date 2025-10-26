dd7a7b83e4603f6a89787d8d2ac75222bf2da510
andremarko <151890840+andremarko@users.noreply.github.com>
2025-10-26 16:03:55 -0300

    feat(moto): adiciona endpoint /all e integração Oracle via procedure JSON

--- src/main/java/com/mottu/mapping/api/repository/MotoRepository.java
+++ src/main/java/com/mottu/mapping/api/repository/MotoRepository.java
@@ -1,10 +1,14 @@
 package com.mottu.mapping.api.repository;
 
 import com.mottu.mapping.api.model.Moto;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@Profile({"oracle", "sqlserver"})
 public interface MotoRepository extends JpaRepository<Moto, Long> {
     Page<Moto> findByPlate(String plate, Pageable pageable);
     Page<Moto> findBySector_SectorId(Long sectorId, Pageable pageable);
